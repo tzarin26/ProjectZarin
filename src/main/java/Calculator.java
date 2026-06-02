b1454a6f73b83a80c99f866c93e85f6dcd72c662
zarin <tasnimz@sheridancollege.ca>
2026-06-01 20:33:11 -0400

    Subtraction method created

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -12,5 +12,11 @@ public class Calculator {
     public double add(double a, double b){
         return a + b;
     }
+    
+    public double sub(double a, double b){
+        return a - b;
+    }
+    
+    
 
 }
